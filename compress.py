a3edccee041eeb0238bec666b7e0dcf7f5db67c3
XiWang <vivian.xi.wang@gmail.com>
2023-02-27 15:56:46 -0800

    added compress

---
+++ compress.py
@@ -0,0 +1,30 @@
+# compress
+# Write a function, compress, that takes in a string as an argument. The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character. Single character occurrences should not be changed.
+
+# 'aaa' compresses to '3a'
+# 'cc' compresses to '2c'
+# 't' should remain as 't'
+
+def compress(s):
+    compressed_string =''
+    count = 1
+    sub_str = s[0]
+    i = 1
+    while i < len(s):
+      if s[i] == sub_str:
+          count += 1
+          if i == len(s) - 1:
+            compressed_string += str(count) + sub_str
+      elif s[i] != sub_str and count == 1:
+          compressed_string += sub_str
+          sub_str = s[i]
+          count = 1
+      elif s[i] != sub_str and count > 1:
+            compressed_string += str(count) + sub_str
+            sub_str = s[i]
+            count = 1    
+            if i ==len(s) - 1 and s[i-1] != s[i]:
+                compressed_string +=s[i]
+      i+=1
+      
+    return compressed_string
